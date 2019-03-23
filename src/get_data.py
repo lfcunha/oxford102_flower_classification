9db252ee33f42072c7d551e1f14dda1bfd106abc
Luis Cunha <lfcunha@gmail.com>
2019-03-23 15:55:31 -0400

    download labels

--- src/get_data.py
+++ src/get_data.py
@@ -19,10 +19,15 @@ base_dir = '/content/gdrive/My\ Drive/102flowers'
 
 
 
+http://www.robots.ox.ac.uk/~vgg/data/flowers/102/imagelabels.mat
+
 # Get image segmentations
 
 !wget -cq  http://www.robots.ox.ac.uk/~vgg/data/flowers/102/102segmentations.tgz
 !mv segmim $base_dir/102flowers/
+
+
+
 
 """
 
@@ -32,3 +37,5 @@ base_dir = '/content/gdrive/My Drive/102flowers/'
 mat = scipy.io.loadmat(base_dir + 'imagelabels.mat')
 
 labels = mat['labels'][0]
+
+
